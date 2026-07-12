1935b529c32722786b29469f6f48c59b328dbf0c
aleksandrmazurin1 <aleksandrmazurin1@gmail.com>
2026-07-12 11:36:49 +0300

    sd

--- Todo_Fund_JS2/main.js
+++ Todo_Fund_JS2/main.js
@@ -3,6 +3,8 @@ let todos = [];
 let users = [];
 
 const list = document.getElementById('todo-list');
+const selector = document.getElementById('user-todo');
+const form = document.getElementById('form');
 
 
 //Attach event 
@@ -14,7 +16,7 @@ function getUserName(id) {
     return user.country;
 }
 
-function printTodo({id, name, completed}) {
+function printTodo({ id, name, completed }) {
     const li = document.createElement('li');
     li.className = 'todo-item';
     li.dataset.id = id;
@@ -24,21 +26,31 @@ function printTodo({id, name, completed}) {
     status.type = 'checkbox';
     status.checked = completed;
 
-    //const delBtn = 
-
+    const delBtn = document.createElement('button');
+    delBtn.innerHTML = 'X';
+    delBtn.className = 'close';
 
     list.prepend(li);
     li.prepend(status);
-} 
+    li.append(delBtn);
+}
 
+function createUserOption(user) {
+    
+    const option = document.createElement('option');
+    option.value = user.id;
+    option.innerText = user.name;
 
+    selector.append(option);
+}
 
 //Event logic
 function initApp() {
     Promise.all([getAllTodos(), getAllUsers()]).then(values => {
         [todos, users] = values;
         console.log(todos, users)
-        todos.forEach(todo => printTodo(todo))
+        todos.forEach(todo => printTodo(todo));
+        users.forEach(user => createUserOption(user))
     });
 }
 
@@ -59,4 +71,20 @@ async function getAllUsers() {
     const data = await response.json();
 
     return data;
+}
+
+async function createTodo (todo) {
+    const response = await fetch('https://fake-json-api.mock.beeceptor.com/todos', {
+        method: 'POST',
+        body: JSON.stringify(todo),
+
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }),
+    const todoId = await response.json();
+
+    console.log(todoId);
+
+    printTodo({id: todoId, ...todo})
 }
